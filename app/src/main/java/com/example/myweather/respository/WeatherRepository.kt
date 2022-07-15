cd011795b8b7176d550013b8f49e8f52ca56a610
Atifsid <atifsiddiqui35@gmail.com>
2022-07-15 11:59:02 +0530

    Fix: Address Error handling and Internet availability

--- app/src/main/java/com/example/myweather/respository/WeatherRepository.kt
+++ app/src/main/java/com/example/myweather/respository/WeatherRepository.kt
@@ -1,22 +1,39 @@
 package com.example.myweather.respository
 
+import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.myweather.api.MyWeatherServiceInterface
 import com.example.myweather.dataModels.MyWeather
+import com.example.myweather.utils.NetworkUtils
+import com.example.myweather.viewmodel.MainViewModel
+import java.io.IOException
 
 //Repository is used to manage our data. It needs access to retrofit service to do so.
-class WeatherRepository(private val myWeatherService: MyWeatherServiceInterface) {
+class WeatherRepository(
+    private val myWeatherService: MyWeatherServiceInterface,
+    private val applicationContext: Context
+) {
 
-    private val weatherLiveData = MutableLiveData<MyWeather>()
+    private val weatherLiveData = MutableLiveData<Response<MyWeather>>()
     //Will be called in MainViewModel.
-    val weather: LiveData<MyWeather>
+    val weather: LiveData<Response<MyWeather>>
         get() = weatherLiveData
 
     suspend fun getWeather(location: String, units: String) {
-        val result = myWeatherService.getWeather(location,units)
-        if(result?.body() != null){
-            weatherLiveData.postValue(result.body())
+        if(NetworkUtils.isInternetAvailable(applicationContext)){
+                val result = myWeatherService.getWeather(location,units)
+                if(result?.body() != null){
+                    weatherLiveData.postValue(Response.Success(result.body()))
+                }
+                else{
+                    weatherLiveData.postValue(Response.Error("Error 404: Location not found"))
+                }
         }
+        else{
+            //handle no internet
+            weatherLiveData.postValue(Response.Error("No Internet"))
+        }
+
     }
 }
--- app/src/main/java/com/example/myweather/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/myweather/viewmodel/MainViewModel.kt
@@ -3,25 +3,36 @@ package com.example.myweather.viewmodel
 import android.view.KeyEvent
 import android.view.View
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.myweather.dataModels.MyWeather
+import com.example.myweather.respository.Response
 import com.example.myweather.respository.WeatherRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.io.IOException
 
 //Parameterized ViewModel so it requires a factory.
 class MainViewModel(private val repository: WeatherRepository) : ViewModel() {
     var location: String? = null
+    var loactionStatus = MutableLiveData<Boolean?>()
 
     fun setOnSearchbtnClick() {
-        viewModelScope.launch(Dispatchers.IO) {
-            //MainViewModel calls getWeather to fetch weather info
-            repository.getWeather(location!!.trim(), "metric")
+        if(location != null){
+                loactionStatus.value = true
+                viewModelScope.launch(Dispatchers.IO) {
+                    //MainViewModel calls getWeather to fetch weather info
+                    repository.getWeather(location!!.trim(), "metric")
+                }
+            }
+        else{
+            loactionStatus.value = false
         }
+
     }
 
-    val weather: LiveData<MyWeather>
+    val weather: LiveData<Response<MyWeather>>
         get() = repository.weather
 
 
